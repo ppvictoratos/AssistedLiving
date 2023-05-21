178c36dfdd2737deb2b5ac2b6998b78e3100ffe6
petiepositivo <pvictoratos@rocketmail.com>
2023-05-21 00:06:59 -0400

    did stuff

--- My Music Board/My Music Board/ContentView.swift
+++ My Music Board/My Music Board/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isSpinning = false //state for spinning wheel
     @State private var isSongCardVisible = false
     @State private var isProfileVisible = false
+    @State private var isSpinning = false //state for spinning wheel
+    @State private var isLocked = false
     @State private var isFlipped = false //state for settings modal
     @State private var profile: Profile = Profile(
         name: "Johnny Boy",
@@ -32,53 +33,67 @@ struct ContentView: View {
             )
     ]
     
-    
     var body: some View {
         ZStack {
             Color.white.edgesIgnoringSafeArea(.all)
             
             VStack {
-                HStack {
-                    Spacer()
-                    
-                    BannerView(text: "Current Song Playing Title")
-                        .offset(x: UIScreen.main.bounds.width * 0.5)
-                    
-                    Spacer()
-                    
-                    BannerView(text: "User Profile Name")
-                        .offset(x: UIScreen.main.bounds.width * 0.8)
-                    
-                    Spacer()
+                ZStack{
+                    CircleSpinnerView(isSpinning: isSpinning, isLocked: isLocked)
+                        .gesture(
+                            DragGesture().onEnded { value in
+                                guard !isLocked else { return }
+                                let velocity = value.predictedEndLocation.x - value.location.x
+                                isSpinning = velocity > 0
+                            }
+                        )
+                        .frame(width: UIScreen.main.bounds.width * 0.9,
+                               height: UIScreen.main.bounds.width * 0.9)
+                                        
+                    Button(action: {
+                        isSongCardVisible.toggle()
+                    }, label: {
+                        Image(systemName: "person.circle")
+                            .font(.system(size: 90))
+                            .padding(50)
+                            .background(Color.red)
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                    })
+                    .padding()
                 }
-            }
-            
-            Spacer()
-            
-            ZStack {
-                CircleView()
                 
-                if isProfileVisible {
-                    ProfileView()
-                        .opacity(isProfileVisible ? 1.0 : 0.0)
-                        .animation(.spring())
+                Button(action: {
+                    isLocked.toggle()
+                }, label: {
+                    Image(systemName: isLocked ? "lock.fill" : "lock.open")
+                        .font(.system(size: 24))
+                        .padding()
+                })
+                
+                Spacer()
+                
+                VStack {
+                    Spacer()
+                    BannerView(
+                        text: "Current Song Playing Title",
+                        color: Color.black
+                    )
+                        .offset(x: UIScreen.main.bounds.width * 0.2)
+            
+                    Spacer()
+                    
+                    BannerView(
+                        text: "User Profile Name",
+                        color: Color.white
+                    )
+                        .offset(x: -UIScreen.main.bounds.width * 0.28)
+                    
+                    Spacer()
+                    
                 }
             }
-            
-            Spacer()
-            
-            Button(action: {
-                isSongCardVisible.toggle()
-            }, label: {
-                Image(systemName: "person.circle")
-                    .font(.system(size: 80))
-                    .padding(30)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .clipShape(Circle())
-            })
-            .padding()
-            
+                
             if isSongCardVisible {
                 SongCardView(song: songs[0])
                     .onTapGesture {
@@ -93,34 +108,47 @@ struct ContentView: View {
 
 struct BannerView: View {
     let text: String
+    let color: Color
+    
     var body: some View {
         Text(text)
-            .font(.headline)
+            .font(.body)
             .foregroundColor(.white)
             .padding()
-            .background(Color.blue)
-            .cornerRadius(10)
+            .background(color)
     }
 }
 
-struct CircleView: View {
+struct CircleSpinnerView: View {
+    let circleCount = 7
+    let circleSize: CGFloat = 45
+    let circleSpacing: CGFloat = 45
+    let spinnerRadius: CGFloat = 130
+    
+    @State private var angle: Angle = .zero
+    
+    var isSpinning: Bool
+    var isLocked: Bool
+    
     var body: some View {
         ZStack {
-            Circle()
-                .frame(width: 240, height: 240)
-                .foregroundColor(.gray)
-            
-            ForEach(0..<7) { index in
-                    Circle()
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.gray)
-                        .position(
-                            x: 120 + 100 * cos((Double(index) * 2 * 2 .pi / 7) - .pi / 2),
-                            y: 120 + 100 * sin((Double(index) * 2 * 2 .pi / 7) - .pi / 2)
-                        )
+            ForEach(0..<circleCount) { index in
+                Circle()
+                    .frame(width: circleSize, height: circleSize)
+                    .foregroundColor(.gray)
+                    .offset(y: -spinnerRadius)
+                    .rotationEffect(angle + .degrees(Double(index) * (360.0 / Double(circleCount))))
             }
         }
-        .padding()
+        .frame(width: spinnerRadius * 2, height: spinnerRadius * 2)
+        .rotationEffect(isSpinning && !isLocked ? angle : .zero)
+        .animation(.spring())
+        .gesture(
+            RotationGesture().onChanged { angle in
+                guard !isLocked else { return }
+                self.angle = angle
+            }
+        )
     }
 }
 
@@ -144,6 +172,7 @@ struct ProfileView: View {
         .padding()
     }
 }
+    
 
 
 struct ContentView_Previews: PreviewProvider {
